2583e08c62b05c4e842770a7c39dd715984a7762
Jax <enhancedjax@gmail.com>
2024-12-23 12:20:34 +0800

    WIP: Tests

--- tests/snapshot.py
+++ tests/snapshot.py
@@ -51,23 +51,24 @@ def travel_to_hill_valley_time():
 
 
 class TestBasic:
-    def test_welcome(self, snap_compare):
+    def test_1_welcome(self, snap_compare):
         assert snap_compare(App(**APP_PARAMS), terminal_size=SIZE_BASIC)
 
-    def test_new_acc_welcome(self, snap_compare):
-        assert snap_compare(
-            App(**APP_PARAMS),
-            terminal_size=SIZE_BASIC,
-            press=["a", "t", "tab", *"123.45", "enter"],
-        )
+    def test_2_new_acc_welcome(self, snap_compare):
+        async def r(pilot: Pilot):
+            await pilot.press("a")  # add
+            await pilot.press("t", "tab")  # name
+            await pilot.press(*"123.45", "enter")  # value
 
-    def test_vertical_layout(self, snap_compare):
+        assert snap_compare(App(**APP_PARAMS), terminal_size=SIZE_BASIC, run_before=r)
+
+    def test_3_vertical_layout(self, snap_compare):
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_VERTICAL,
         )
 
-    def test_jump_screen(self, snap_compare):
+    def test_4_jump_screen(self, snap_compare):
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_BASIC,
@@ -79,28 +80,43 @@ class TestBasic:
 
 
 class TestAccounts:
-    def test_acc_screen(self, snap_compare):
+    def test_5_acc_screen(self, snap_compare):
         assert snap_compare(App(**APP_PARAMS), terminal_size=SIZE_TEST, press=["a"])
 
-    def test_new_acc_home(self, snap_compare):
+    def test_6_new_acc_home(self, snap_compare):
+        async def r(pilot: Pilot):
+            await pilot.press("v", "a")  # jump to accounts
+            await pilot.wait_for_animation()
+            await pilot.press("a")  # add
+            await pilot.press(*"t2", "enter")  # name
+
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
-            press=["v", "a", "a", "t", "2", "tab", "enter"],
+            run_before=r,
         )
 
-    def test_delete_acc(self, snap_compare):
+    def test_7_delete_acc(self, snap_compare):
+        async def r(pilot: Pilot):
+            await pilot.press("v", "a")  # jump to accounts
+            await pilot.press("d", "enter")  # delete
+
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
-            press=["v", "a", "d", "enter"],
+            run_before=r,
         )
 
-    def test_edit_acc(self, snap_compare):
+    def test_8_edit_acc(self, snap_compare):
+        async def r(pilot: Pilot):
+            await pilot.press("v", "a")  # jump to accounts
+            await pilot.press("e")  # edit
+            await pilot.press("tab", *"123.234", "enter")  # edit beg. balance
+
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
-            press=["v", "a", "e", "tab", *"123.234", "enter"],
+            run_before=r,
         )
 
 
@@ -108,47 +124,57 @@ class TestAccounts:
 
 
 class TestCategories:
-    def test_categories(self, snap_compare):
+    def test_9_categories(self, snap_compare):
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
             press=["c"],
         )
 
-    def test_new_category_screen(self, snap_compare):
+    def test_10_new_category_screen(self, snap_compare):
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
             press=["c", "a"],
         )
 
-    def test_new_category(self, snap_compare):
+    def test_11_new_category(self, snap_compare):
+        async def r(pilot: Pilot):
+            await pilot.press("c")  # show categories modal
+            await pilot.press("a")  # add
+            await pilot.press("t", "tab")  # name
+            await pilot.press("tab", "tab", "enter")  # nature, color, save
+
+        # todo: scroll to bottom
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
-            press=[
-                "c",
-                "a",
-                "t",
-                "tab",
-                "tab",
-                "tab",
-                "enter",
-            ],  # todo: scroll bottom to see new cat
+            run_before=r,
         )
 
-    def test_new_category_subcategory(self, snap_compare):
+    def test_12_new_category_subcategory(self, snap_compare):
+        async def r(pilot: Pilot):
+            await pilot.press("c")  # show categories modal
+            await pilot.press("up")  # select last created
+            await pilot.press("s")  # add subcategory
+            await pilot.press("t", "tab")  # name
+            await pilot.press("tab", "tab", "enter")  # nature, color, save
+
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
-            press=["c", "up", "s", "t", "tab", "tab", "tab", "enter"],
+            run_before=r,
         )
 
-    def test_delete_category(self, snap_compare):
+    def test_13_delete_category(self, snap_compare):
+        async def r(pilot: Pilot):
+            await pilot.press("c")  # show categories modal
+            await pilot.press("d", "enter")  # delete
+
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
-            press=["c", "d", "enter"],
+            run_before=r,
         )
 
 
@@ -156,28 +182,32 @@ class TestCategories:
 
 
 class TestTemplates:
-    def test_templates(self, snap_compare):
+    def test_14_templates(self, snap_compare):
+        async def r(pilot: Pilot):
+            await pilot.press("v", "t")  # jump to templates
+            await pilot.press("a")  # add
+            await pilot.press("t", "tab")  # name
+            await pilot.press("tab")  # category
+            await pilot.press(*"123")  # enter value
+            await pilot.press("tab")  # account
+            await pilot.press("tab", "space", "enter")  # switch to income, save
+
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
-            press=[
-                "v",
-                "t",
-                "a",
-                "t",
-                "tab",
-                "tab",
-                *"123",
-                "tab",
-                "tab",
-                "space",
-                "enter",
-            ],
+            run_before=r,
         )
 
-    def test_edit_template(self, snap_compare):
+    def test_15_edit_template(self, snap_compare):
+        async def r(pilot: Pilot):
+            await pilot.press("v", "t")  # jump to templates
+            await pilot.press("tab")  # select first template
+            await pilot.press("e")  # edit
+            await pilot.press(*"est")  # edit name
+            await pilot.press("enter")  # save
+
         assert snap_compare(
             App(**APP_PARAMS),
             terminal_size=SIZE_TEST,
-            press=["v", "t", "tab", "e", *"est", "enter"],
+            run_before=r,
         )
