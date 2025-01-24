39ad062c9e5a6f79eeeb09f346775534832334be
Jax <enhancedjax@gmail.com>
2025-01-24 14:18:32 +0800

    feat: budgeting 2/2

--- src/bagels/components/modules/budgets.py
+++ src/bagels/components/modules/budgets.py
@@ -27,12 +27,12 @@ class Budgets(Static):
     def on_mount(self) -> None:
         self.rebuild()
 
-    def _write_state(self, key: str, value: float) -> bool:
+    def _write_state(self, key: str, value: str) -> bool:
         current_value = CONFIG.state
         for part in key.split("."):
             current_value = getattr(current_value, part)
-        if value and current_value != value:
-            write_state(key, value)
+        if value != "" and current_value != float(value):
+            write_state(key, float(value))
             return True
         return False
 
@@ -40,22 +40,18 @@ class Budgets(Static):
         updated = False
         if event.input.id == "savings-input":
             if self.savings_assess_metric.startswith("percentage"):
-                updated = self._write_state(
-                    "budgeting.savings_percentage", float(event.value)
-                )
+                updated = self._write_state("budgeting.savings_percentage", event.value)
             else:
-                updated = self._write_state(
-                    "budgeting.savings_amount", float(event.value)
-                )
+                updated = self._write_state("budgeting.savings_amount", event.value)
 
         elif event.input.id == "wants-input":
             if self.wants_spending_assess_metric.startswith("percentage"):
                 updated = self._write_state(
-                    "budgeting.wants_spending_percentage", float(event.value)
+                    "budgeting.wants_spending_percentage", event.value
                 )
             else:
                 updated = self._write_state(
-                    "budgeting.wants_spending_amount", float(event.value)
+                    "budgeting.wants_spending_amount", event.value
                 )
         if updated:
             self.rebuild()
@@ -119,21 +115,37 @@ class Budgets(Static):
             else CONFIG.state.budgeting.savings_amount,
             CONFIG.defaults.round_decimals,
         )
-        expenses_must = dynamic_cache(
-            get_period_figures,
-            isIncome=False,
-            offset=offset,
-            offset_type="month",
-            nature=Nature.MUST,
+        want_quota = round(
+            (net_income - amount_to_save)
+            * CONFIG.state.budgeting.wants_spending_percentage
+            if self.wants_spending_assess_metric.startswith("percentage")
+            else CONFIG.state.budgeting.wants_spending_amount,
+            CONFIG.defaults.round_decimals,
         )
-        expenses_need = dynamic_cache(
-            get_period_figures,
-            isIncome=False,
-            offset=offset,
-            offset_type="month",
-            nature=Nature.NEED,
+        expenses_must = round(
+            dynamic_cache(
+                get_period_figures,
+                isIncome=False,
+                offset=offset,
+                offset_type="month",
+                nature=Nature.MUST,
+            ),
+            CONFIG.defaults.round_decimals,
         )
-        expenses_want = net_expenses - expenses_must - expenses_need
+        expenses_need = round(
+            dynamic_cache(
+                get_period_figures,
+                isIncome=False,
+                offset=offset,
+                offset_type="month",
+                nature=Nature.NEED,
+            ),
+            CONFIG.defaults.round_decimals,
+        )
+        expenses_want = round(
+            net_expenses - expenses_must - expenses_need,
+            CONFIG.defaults.round_decimals,
+        )
 
         self.app.log(
             f"Data: net_income={net_income}, net_expenses={net_expenses}, amount_to_save={amount_to_save}, expenses_must={expenses_must}, expenses_need={expenses_need}, expenses_want={expenses_want}"
@@ -156,7 +168,7 @@ class Budgets(Static):
         row1.styles.grid_columns = row1_columns
 
         row2 = income_bar_container.query_one("#row-2")
-        row2.styles.grid_columns = f"{p_expenses}%  1fr"
+        row2.styles.grid_columns = f"{p_expenses}% 1fr"
 
         label_spent_amount = income_bar_container.query_one("#label-spent-amount")
         label_spent_amount.update(str(net_expenses))
@@ -172,15 +184,37 @@ class Budgets(Static):
 
         row3 = income_bar_container.query_one("#row-3")
         row3.display = not not net_expenses  # only display if there are expenses
-        income_bar_container.styles.height = 5 if not not net_expenses else 4
+
+        row4 = income_bar_container.query_one("#row-4")
+        row4.display = not not net_expenses
+
+        row5 = income_bar_container.query_one("#row-5")
+        row5.display = not not net_expenses
 
         if not not net_expenses:
-            p_must = round(expenses_must / net_expenses * 100)
-            p_need = round(expenses_need / net_expenses * 100)
-            row3_columns = f"{p_must}% {p_need}% 1fr"
-            self.app.log(row3_columns)
-            row3.styles.grid_columns = row3_columns
-            row3.styles.width = f"{p_expenses}%"
+            p_tospend = round(100 - p_saving)
+            row3.styles.width = f"{p_tospend}%"
+
+            p_quota = round(100 - p_saving - p_expenses)
+            p_must = round(expenses_must / net_expenses * p_expenses)
+            p_need = round(expenses_need / net_expenses * p_expenses)
+            p_want = round(expenses_want / net_expenses * p_expenses)
+            row4_columns = f"{p_must}% {p_need}% {p_quota}% {p_want}% 1fr"
+            self.app.log(row4_columns)
+            row4.styles.grid_columns = row4_columns
+
+            row3.styles.grid_columns = f"{p_must}% {p_need}% 1fr"
+            label_spent_must_amount = row3.query_one("#label-spent-must-amount")
+            label_spent_must_amount.update(str(expenses_must))
+            label_spent_need_amount = row3.query_one("#label-spent-need-amount")
+            label_spent_need_amount.update(str(expenses_need))
+            label_spent_want_amount = row3.query_one("#label-want-remaining-amount")
+            label_spent_want_amount.update(f"{str(expenses_want)} / {str(want_quota)}")
+
+            row5.styles.width = f"{p_tospend}%"
+            bar_want_quota = row5.query_one("#bar-must-quota")
+            p_want_quota = round(want_quota / (net_income - amount_to_save) * 100)
+            bar_want_quota.styles.width = f"{p_want_quota}%"
 
     # region View
     # --------------- View --------------- #
@@ -189,15 +223,27 @@ class Budgets(Static):
             yield Label("Savings: ", id="savings-label")
             yield Button(
                 "% Period Income",
-                tooltip="percentagePeriodIncome",
+                tooltip="Save a percentage of this period's income. Will temporarily use last period's value if income not yet recorded this period. Will use fallback value otherwise.\n\nValue: \"percentagePeriodIncome\"",
                 id="savings-percentagePeriodIncome",
             )
-            yield Button("Set amount", tooltip="setAmount", id="savings-setAmount")
+            yield Button(
+                "Set amount",
+                tooltip='Save a set amount each period.\n\nValue: "setAmount"',
+                id="savings-setAmount",
+            )
             yield Input(placeholder="0.0", id="savings-input")
         with Horizontal(id="wants-row", classes="config-rows"):
             yield Label("Wants: ", id="wants-label")
-            yield Button("% Expenses", tooltip="percentage", id="wants-percentage")
-            yield Button("Set amount", tooltip="setAmount", id="wants-setAmount")
+            yield Button(
+                "% Quota",
+                tooltip='Limit spending on WANTs each period by a percentage of the amount left after savings.\n\nValue: "percentage"',
+                id="wants-percentageQuota",
+            )
+            yield Button(
+                "Set amount",
+                tooltip='Limit spending on WANTs each period by a set amount.\n\nValue: "setAmount"',
+                id="wants-setAmount",
+            )
             yield Input(
                 placeholder="0.0",
                 id="wants-input",
@@ -221,11 +267,23 @@ class Budgets(Static):
                     yield Label("╰ Save: ")
                     yield Label("0.0", id="label-save-amount")
             with Container(id="row-3"):
+                with Horizontal():
+                    yield Label("╭ Must: ")
+                    yield Label("0.0", id="label-spent-must-amount")
+                with Horizontal():
+                    yield Label("╭ Need: ")
+                    yield Label("0.0", id="label-spent-need-amount")
+                with Horizontal(id="want"):
+                    yield Label("Want: ")
+                    yield Label("(0.0 / 0.0)", id="label-want-remaining-amount")
+                    yield Label(" ╮")
+            with Container(id="row-4"):
                 yield Static(id="bar-spent-must")
                 yield Static(id="bar-spent-need")
+                yield Static(id="bar-spent-quota")
                 yield Static(id="bar-spent-want")
-                yield Label("Must")
-                yield Label("Need")
-                yield Label("Want")
+                yield Label("Saved")
+            with Container(id="row-5"):
+                yield Static(id="bar-must-quota")
         with Container(classes="empty-bar"):
             yield Label("Nothing to display. Budgeting depends on your income!")
